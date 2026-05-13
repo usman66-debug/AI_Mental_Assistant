9f5a3eee859988cbfc93976184c70e7a44a3bee4
yigiuwole <3251013822@qq.com>
2026-05-13 19:18:03 +0800

    表单数据的双向绑定

--- src/components/backend/TableSearch.vue
+++ src/components/backend/TableSearch.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from 'vue'
+
+const formData = ref({})
 const props = defineProps({
   formItem: {
     type: Array,
@@ -15,10 +18,24 @@ const isComponent = (type) => {
 </script>
 
 <template>
-  <el-form>
+  <el-form :model="formData">
     <template v-for="item in props.formItem" :key="item.prop">
       <el-form-item :label="item.label" :prop="item.prop">
-        <component :is="isComponent(item.type)" :placeholder="item.placeholder"></component>
+        <component
+          v-model="formData[item.prop]"
+          :is="isComponent(item.type)"
+          :placeholder="item.placeholder"
+        >
+          <template v-if="item.type === 'select'">
+            <el-option label="全部" value="0"></el-option>
+            <el-option
+              v-for="opt in item.options"
+              :key="opt.value"
+              :label="opt.label"
+              :value="opt.value"
+            ></el-option>
+          </template>
+        </component>
       </el-form-item>
     </template>
   </el-form>

d3a3ab28f7196aaedff45aa106d03ab096fb5a11
yigiuwole <3251013822@qq.com>
2026-05-15 10:48:09 +0800

    登录页面LoginView表单效果实现

--- src/views/backend/AuthLayoutCom/LoginView.vue
+++ src/views/backend/AuthLayoutCom/LoginView.vue
@@ -1,1 +1,99 @@
-<template>我是登录页</template>
+<script setup>
+import { ref } from 'vue'
+
+const rules = ref({
+  username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+})
+
+const formData = ref({
+  username: '',
+  password: '',
+})
+const ruleFormRef = ref(null)
+
+//表单提交
+const submitForm = async () => {
+  if (!ruleFormRef.value) {
+    return
+  }
+  //validate方法会返回一个Promise，Promise的resolve方法会传递valid和fields两个参数，valid表示当前表单校验状态，fields表示当前表单元素的值
+  await ruleFormRef.value.validate((valid, fields) => {
+    if (valid) {
+      console.log(fields)
+    }
+  })
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">
+      <div class="back-home">
+        <el-icon><Back /></el-icon>
+        <span>返回首页</span>
+      </div>
+      <div class="title-text">
+        <h2>登录您的账户</h2>
+        <p>请输入您的登录信息</p>
+      </div>
+    </div>
+    <div class="form-container">
+      <el-form :model="formData" ref="ruleFormRef" :rules="rules" label-position="top">
+        <el-form-item label="用户名或邮箱" prop="username">
+          <el-input
+            v-model="formData.username"
+            size="large"
+            placeholder="请输入用户名或邮箱"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input
+            v-model="formData.password"
+            size="large"
+            placeholder="请输入密码"
+            type="password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <el-button class="btn" size="large" type="primary" @click="submitForm">点击登录</el-button>
+      </el-form>
+      <div class="footer">
+        <p>还没有账户？<router-link to="/auth/register">去注册</router-link></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 384px;
+  .title {
+    .back-home {
+      margin-bottom: 60px;
+    }
+    .title-text {
+      text-align: center;
+      h2 {
+        font-size: 36px;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 18px;
+        color: #6b7280;
+      }
+    }
+  }
+  .form-container {
+    margin-top: 30px;
+    .btn {
+      margin-top: 40px;
+      width: 100%;
+    }
+    .footer {
+      text-align: center;
+      padding: 30px;
+    }
+  }
+}
+</style>

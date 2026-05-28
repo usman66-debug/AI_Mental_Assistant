eb4e6136baf1abe44f451fc16fe987ac8e505761
yigiuwole <3251013822@qq.com>
2026-05-28 18:18:19 +0800

    后台用户登出功能以及路由前置守卫配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { ElMessage } from 'element-plus'
 //前台页面后台页面风格迥异，建议写两个不同的路由配置
 const backendRoutes = [
   {
     path: '/back',
+    redirect: '/back/dashboard',
     component: () => import('@/components/backend/BackendLayout.vue'),
     children: [
       {
@@ -60,9 +62,41 @@ const backendRoutes = [
     ],
   },
 ]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: backendRoutes,
+})
+
+//配置路由前置守卫
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  //判断当前用户是否登录
+  if (token) {
+    //判断是前台用户还是后台用户
+    const userInfo = JSON.parse(localStorage.getItem('userinfo'))
+    if (userInfo.userType === 2) {
+      //后台用户，直接放行
+      if (to.path.startsWith('/back')) {
+        //放行到相应页面
+        next()
+      } else {
+        next('/back/dashboard')
+      }
+    } else if (userInfo.userType === 1) {
+      //前台用户，直接放行
+    }
+  } else {
+    //没有登录的话，判断想访问的是后台页面还是前台页面
+    //如果是后台页面，提示用户先登录
+    if (to.path.startsWith('/back')) {
+      ElMessage.error('请先登录')
+      next('/auth/login')
+    } else {
+      //前台用户，直接放行
+      next()
+    }
+  }
 })
 
 export default router
